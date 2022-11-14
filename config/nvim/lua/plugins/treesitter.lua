ed4562dbd2bf87aa2f620ae16ee0b9314c9852cb
Rochana Atapattu <rochana.atapattu2@gmail.com>
2022-11-15 00:58:12 +0530

    test

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,55 +1,42 @@
-require("nvim-treesitter.configs").setup {
-  ensure_installed = {"bash", "c", "cpp", "css", "html", "javascript", "lua", "python", "rust", "typescript", "vim"},
+-- import nvim-treesitter plugin safely
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+  return
+end
+
+-- configure treesitter
+treesitter.setup({
+  -- enable syntax highlighting
   highlight = {
     enable = true,
-    use_languagetree = true
   },
-  context_commentstring = {
-    enable = true
+  -- enable indentation
+  indent = { enable = true },
+  -- enable autotagging (w/ nvim-ts-autotag plugin)
+  autotag = { enable = true },
+  -- ensure these language parsers are installed
+  ensure_installed = {
+    "json",
+    "javascript",
+    "typescript",
+    "tsx",
+    "yaml",
+    "html",
+    "css",
+    "markdown",
+    "svelte",
+    "graphql",
+    "bash",
+    "lua",
+    "vim",
+    "dockerfile",
+    "gitignore",
+    "hcl",
+    "go",
+    "gomod",
+    "python",
+    "dockerfile",
   },
-  indent = {enable = true},
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = 1000
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- automatically jump forward to matching textobj
-      keymaps = {
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner"
-      }
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ["<leader>a"] = "@parameter.inner"
-      },
-      swap_previous = {
-        ["<leader>A"] = "@parameter.inner"
-      }
-    }
-  },
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25,
-    persist_queries = false,
-    keybindings = {
-      toggle_query_editor = "o",
-      toggle_hl_groups = "i",
-      toggle_injected_languages = "t",
-      toggle_anonymous_nodes = "a",
-      toggle_language_display = "I",
-      focus_language = "f",
-      unfocus_language = "F",
-      update = "R",
-      goto_node = "<cr>",
-      show_help = "?"
-    }
-  }
-}
+  -- auto install above language parsers
+  auto_install = true,
+})
