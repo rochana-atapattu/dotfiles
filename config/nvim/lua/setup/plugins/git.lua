4424adddf2c327931e4c17a0dbeaf653f67515ea
Rochana Atapattu <rochana.atapattu2@gmail.com>
2024-01-13 00:40:33 +0530

    keybindings

--- config/nvim/lua/setup/plugins/git.lua
+++ config/nvim/lua/setup/plugins/git.lua
@@ -43,10 +43,15 @@ return {
             end, { expr = true })
 
             -- Actions
-            map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
-            map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+            map("n", "<leader>hs", gs.stage_hunk)
+            map("n", "<leader>hr", gs.reset_hunk)
+            map("v", "<leader>hs", function()
+              gs.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
+            end)
+            map("v", "<leader>hr", function()
+              gs.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
+            end)
             map("n", "<leader>hS", gs.stage_buffer)
-            map("n", "<leader>ha", gs.stage_hunk)
             map("n", "<leader>hu", gs.undo_stage_hunk)
             map("n", "<leader>hR", gs.reset_buffer)
             map("n", "<leader>hp", gs.preview_hunk)
