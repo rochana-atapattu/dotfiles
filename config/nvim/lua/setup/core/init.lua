8baa745a470380fb3720108158747be6300ee713
Rochana Atapattu <rochana.atapattu2@gmail.com>
2024-01-13 00:39:37 +0530

    add delete file functionality

--- config/nvim/lua/setup/core/init.lua
+++ config/nvim/lua/setup/core/init.lua
@@ -13,13 +13,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+local function confirm_and_delete_buffer()
+  local confirm = vim.fn.confirm("Delete buffer and file?", "&Yes\n&No", 2)
 
--- Options through Telescope
-vim.api.nvim_set_keymap("n", "<Leader><tab>", "<Cmd>lua require('telescope.builtin').commands()<CR>", {noremap=false})
-
--- Fterm
-vim.api.nvim_set_keymap("n", "<leader>tt", ":lua require('FTerm').toggle()<CR>", {noremap=true})
-vim.api.nvim_set_keymap("t", "<leader>tt", '<C-\\><C-n>:lua require("FTerm").toggle()<CR>', {noremap=true})
+  if confirm == 1 then
+    os.remove(vim.fn.expand "%")
+    vim.api.nvim_buf_delete(0, { force = true })
+  end
+end
+vim.keymap.set('n', '<leader>df', confirm_and_delete_buffer,{desc = "Delete file"})
 
 -- Noice
 vim.api.nvim_set_keymap("n", "<leader>nn", ":NoiceDismiss<CR>", {noremap=true})
