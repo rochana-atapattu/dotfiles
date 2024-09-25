3cd7a659046c0c706de5e00d844909a5de4128e8
rochana <31321610+rochana-atapattu@users.noreply.github.com>
2024-09-25 14:23:16 +0530

    nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,6 +3,8 @@
 {
 
   home = {
+    username = "rochana";
+    homeDirectory = "/Users/rochana";
     stateVersion = "24.05"; # Please read the comment before changing.
 
     # The home.packages option allows you to install Nix packages into your
@@ -13,14 +15,38 @@
       pkgs.markdown-oxide
       pkgs.nixd
       pkgs.ripgrep
+      pkgs.hello
     ];
 
     # Home Manager is pretty good at managing dotfiles. The primary way to manage
     # plain files is through 'home.file'.
     file = {
+      ".zshrc".source = ../zsh/zshrc.symlink;
+      ".zshenv".source = ../zsh/zshenv.symlink;
+      ".zprofile".source = ../zsh/zprofile.symlink;
     };
 
     sessionVariables = {
+    };
+
+    sessionPath = [
+    "/run/current-system/sw/bin"
+      "$HOME/.nix-profile/bin"
+    ];
+  };
+
+  programs = {
+    home-manager.enable = true;
+    
+    zsh = {
+      enable = true;
+      initExtra = ''
+        # Add any additional configurations here
+        export PATH=/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
+        if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+          . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+        fi
+      '';
     };
   };
 
